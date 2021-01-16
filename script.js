ba94496334f8be920e344e15915ba10c9a4232df
annsudhart <asudhart@ucsd.edu>
2021-01-16 15:59:47 -0800

    created function to scrape price, title, image

--- script.js
+++ script.js
@@ -8,3 +8,26 @@ testdiv.setAttribute('id', 'honeycomb');
 testdiv.innerHTML = "injected!!!";
 document.getElementById('price').insertAdjacentElement('beforebegin', testdiv);
 console.log('done inserting!');
+/*let honeycombUrl = chrome.extension.getURL('small.html');
+console.log(honeycombUrl);
+fetch(honeycombUrl, {
+    "Access-Control-Allow-Origin": "*",
+    "Content-Type": "text/plain"
+},).then(data => data.text()).then(res => {console.log(res)})*/
+
+async function getSponsoredProductFromUrl(url) {
+    var sponsoredProductHtml = '';
+    sponsoredProductHtml = await fetch(url).then(res=>res.text()).then(data=> sponsoredProductHtml = data);
+    let data = {};
+    let parser = new DOMParser();
+    let sponsoredProductElement = parser.parseFromString(sponsoredProductHtml, 'text/html');
+    // console.log(sponsoredProductElement);
+    data['title'] = sponsoredProductElement.getElementById('productTitle').innerText.trim();
+    data['image'] = sponsoredProductElement.getElementById('imgTagWrapperId').firstChild.nextSibling.getAttribute('src');
+    data['price'] = sponsoredProductElement.getElementById('priceblock_saleprice') ? sponsoredProductElement.getElementById('priceblock_saleprice').innerText : sponsoredProductElement.getElementById('priceblock_ourprice').innerText;
+    data['trim'] = sponsoredProductElement.getElementById('acrPopover').innerText.trim();
+    console.log(data);
+}
+
+// test: remove this line of code before publishing
+getSponsoredProductFromUrl('https://www.amazon.com/Tiny-Footprint-Coffee-Organic-Medium/dp/B009PIFCCO/')
