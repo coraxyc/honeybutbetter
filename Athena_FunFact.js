08f15a6bfa9d3346185fba69fa683ef6e054524a
annsudhart <asudhart@ucsd.edu>
2021-01-16 23:25:17 -0800

    random fun fact generator made

--- Athena_FunFact.js
+++ Athena_FunFact.js
@@ -1,2 +1,15 @@
-//read tips.json
-const tips = require('tips.json')
+// read tips.json
+// const tips = require('tips.json'); @Athena: require is a method in the node.js framework, not plain js,
+// feel free to ask me for help for loading in a json file
+
+async function getRandomFunFact() {
+    funFactsUrl = chrome.runtime.getURL('tips.json');
+    var funFactsFile = {};
+    funFactsFile = await fetch(funFactsUrl).then(res=>{return res.json()}).then(data=> funFactsFile = data);
+    let keywordId = Math.floor(Math.random() * funFactsFile.allKeywords.length);
+    let keyword = funFactsFile.allKeywords[keywordId];
+    let funFactId = Math.floor(Math.random() * funFactsFile.keywords[keyword].length);
+    return funFactsFile.keywords[keyword][funFactId];
+}
+
+getRandomFunFact().then(res => console.log(`${res['description']}\nSource: ${res['source']}`));
